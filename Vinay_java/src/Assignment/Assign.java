1b5329012cd9d59d8e78598ae4c73e31c8db8f1b
Vinay.R <Vinay.R@DESKTOP-LM2G8RH>
2022-10-23 14:59:57 +0530

    new

---
+++ Vinay_java/src/Assignment/Assign.java
@@ -0,0 +1,28 @@
+package Assignment;
+
+public class Assign {
+
+	
+	public static void main(String[] args) {
+		int a=1000;
+		int c;
+		c=a;
+		System.out.println(c);
+		
+int i;
+i=100;
+i=i+1;
+System.out.println(i);
+int j;
+j=100;
+j=j-1;
+System.out.println(j);
+
+int k;k=25;k++;
+System.out.println(k);
+
+
+	
+	}
+}
+
