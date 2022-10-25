d227c9fed0a992d2762f0eab6b774ff2f958a872
Vinay.R <Vinay.R@DESKTOP-LM2G8RH>
2022-10-25 06:37:41 +0530

    second

---
+++ Vinay_java/src/operator/operators.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class operators {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=200;
+		
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+	}
+
+}
